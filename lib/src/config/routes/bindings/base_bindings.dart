4545451c00d9eec9a36132641ed0f706a40081af
Peyman <peymantaghipour.fp@gmail.com>
2025-01-20 14:15:18 +0330

    product list page

--- lib/src/config/routes/bindings/base_bindings.dart
+++ lib/src/config/routes/bindings/base_bindings.dart
@@ -1,14 +1,22 @@
 import 'package:get/get.dart';
 import 'package:main_project/src/domain/controllers/base_controller.dart';
+import 'package:main_project/src/domain/controllers/slide_controller.dart';
+import 'package:main_project/src/domain/pages/dashboard/dashboard_page.dart';
 import 'package:main_project/src/domain/pages/home/home_page.dart';
+import 'package:main_project/src/domain/pages/product/product_list_page.dart';
 import 'package:main_project/src/domain/pages/profile/profile_page.dart';
+import 'package:main_project/src/domain/pages/tools/tools_page.dart';
 
 class BaseBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(()=>BaseController());
+    Get.lazyPut(()=>SlideController());
     Get.lazyPut(()=>ProfilePage());
     Get.lazyPut(()=>HomePage());
+    Get.lazyPut(()=>DashboardPage());
+    Get.lazyPut(()=>ProductListPage());
+    Get.lazyPut(()=>ToolsPage());
   }
 
 }
--- lib/src/domain/pages/product/product_list_page.dart
+++ lib/src/domain/pages/product/product_list_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:main_project/src/config/consts/app_color.dart';
+import 'package:main_project/src/domain/controllers/slide_controller.dart';
+import 'package:get/get.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class ProductListPage extends StatefulWidget {
-  const ProductListPage({super.key});
+class ProductListPage extends StatelessWidget {
+  ProductListPage({super.key});
 
-  @override
-  State<ProductListPage> createState() => _ProductListPageState();
-}
+  final SlideController slideController = Get.find<SlideController>();
 
-class _ProductListPageState extends State<ProductListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +16,87 @@ class _ProductListPageState extends State<ProductListPage> {
         title: Text('لیست محصولات'),
         centerTitle: true,
       ),
-      body: Container(
-
+      body: SafeArea(
+        child: SizedBox(
+          height: Get.height,
+          width: Get.width,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Obx(() {
+                  return CarouselSlider.builder(
+                      itemCount: slideController.images.length,
+                      itemBuilder: (context, index, realIndex) {
+                        return Container(
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(image: AssetImage(slideController
+                                .images[index]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                      options: CarouselOptions(
+                        height: Get.height * 0.25,
+                        autoPlay: false,
+                        enlargeCenterPage: true,
+                        onPageChanged: (index, reason) {
+                          slideController.updateIndex(index);
+                        },
+                      )
+                  );
+                }),
+                Obx(() {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: slideController.images.map((i) {
+                      int index = slideController.images.indexOf(i);
+                      return Container(
+                        width: 8,
+                        height: 8,
+                        margin: EdgeInsets.symmetric(horizontal: 3,vertical: 10),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: slideController.currentIndex.value == index ?
+                            AppColor.blueColor :
+                            AppColor.inputColor
+                        ),
+                      );
+                    }
+                    ).toList(),
+                  );
+                }),
+                    SizedBox(
+                      height: Get.height*.5,
+                      child: GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            crossAxisSpacing: 5,
+                            mainAxisSpacing: 5
+                          ),
+                          itemCount: slideController.images.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: AssetImage(slideController.images[index]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          },
+                      ),
+                    ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
